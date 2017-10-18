9fee7c1dc150d7dd21eed6977c6fb66d83662fb4
Martin Petráček <martin.petracek@nic.cz>
2017-10-18 19:04:39 +0200

    monitor: split event handling into multiple functions

--- monitor.py
+++ monitor.py
@@ -29,6 +29,76 @@ def timestamp2unixtime(timestamp):
     timestamp = timestamp*1.0 + dt.microsecond*1.0/1000000
     return timestamp
 
+def handle_dns(data, c):
+    if data['dns']['type'] == 'answer' and 'rrtype' in data['dns'].keys() and data['dns']['rrtype'] in ('A', 'AAAA', 'CNAME'):
+        logging.debug('Saving DNS data')
+        c.execute('INSERT INTO dns VALUES (?,?,?,?,?)',
+                    (timestamp2unixtime(data['timestamp']),
+                    data['dest_ip'], data['dns']['rrname'], data['dns']['rrtype'],
+                    data['dns']['rdata']))
+
+def handle_flow(data, c):
+    if data['proto'] not in ['TCP', 'UDP']:
+        return
+    if 'app_proto' not in data.keys():
+        data['app_proto'] = '?'
+    if data['app_proto'] in ['failed', 'dns'] or int(data['flow']['bytes_toserver'])==0 or int(data['flow']['bytes_toclient'])==0:
+        c.execute('DELETE FROM traffic WHERE flow_id = ?', (data['flow_id'],))
+        if c.rowcount!=1:
+            logging.debug("Can't delete flow")
+    else:
+        c.execute('UPDATE traffic SET duration = ?, app_proto = ?, bytes_send = ?, bytes_received = ?, flow_id = NULL WHERE flow_id = ?', (int(timestamp2unixtime(data['flow']['end'])-timestamp2unixtime(data['flow']['start'])), data['app_proto'], data['flow']['bytes_toserver'], data['flow']['bytes_toclient'], data['flow_id']))
+        if c.rowcount!=1:
+            logging.debug("Can't update flow")
+
+def handle_tls(data, c):
+    hostname = ''
+    if 'sni' in data['tls'].keys():
+        hostname = data['tls']['sni']
+    elif 'subject' in data['tls'].keys():
+        hostname = data['tls']['subject']
+        #get only CN from suject
+        m = re.search('(?<=CN=)[^,]*', hostname)
+        if m:
+                hostname = m.group(0)
+    if not hostname:
+        return
+    c.execute('UPDATE traffic SET app_hostname = ?, app_proto = "tls" WHERE flow_id = ?', (hostname, data['flow_id']))
+    if c.rowcount!=1:
+        logging.debug("Can't update flow")
+
+def handle_http(data, c):
+    if 'hostname' not in data['http'].keys():
+        return
+    c.execute('UPDATE traffic SET app_hostname = ?, app_proto = "http" WHERE flow_id = ?', (data['http']['hostname'], data['flow_id']))
+    if c.rowcount!=1:
+        logging.debug("Can't update flow")
+
+def handle_flow_start(data, c):
+    if data['proto'] not in ['TCP', 'UDP']:
+        return
+    if 'app_proto' not in data.keys():
+        data['app_proto'] = '?'
+    if data['app_proto'] in ['failed', 'dns']:
+        return
+    c.execute('INSERT INTO traffic (flow_id, start, src_mac, src_ip, src_port, dest_ip, dest_port, proto, app_proto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                (data['flow_id'], timestamp2unixtime(data['flow']['start']),
+                data['ether']['src'], data['src_ip'],
+                data['src_port'], data['dest_ip'], data['dest_port'],
+                data['proto'], data['app_proto']))
+
+def exit_gracefully(signum, frame):
+    global con, conntrack
+    conntrack.terminate()
+    time.sleep(1)
+    if not con:
+        return
+    con.commit()
+    if con:
+         con.close()
+    conntrack.kill()
+    sys.exit(0)
+
 if not os.path.isfile('/var/lib/pakon.db'):
     subprocess.call(['/usr/bin/python3', '/usr/libexec/pakon-light/create_db.py'])
 con = sqlite3.connect('/var/lib/pakon.db')
@@ -40,19 +110,6 @@ try:
     con.commit()
 except:
     logging.debug('Error cleaning flow_id')
-
-
-def exit_gracefully(signum, frame):
-    global c, con
-    conntrack.terminate()
-    time.sleep(1)
-    if not con:
-        return
-    con.commit()
-    if con:
-         con.close()
-    conntrack.kill()
-    sys.exit(0)
 
 signal.signal(signal.SIGINT, exit_gracefully)
 signal.signal(signal.SIGTERM, exit_gracefully)
@@ -89,66 +146,17 @@ while True:
             data['ether']={}
             data['ether']['src']=''
         if data['event_type'] == 'dns' and data['dns']:
-            logging.debug('Got dns!')
-            if data['dns']['type'] == 'answer' and 'rrtype' in data['dns'].keys() and data['dns']['rrtype'] in ('A', 'AAAA', 'CNAME'):
-                logging.debug('Saving DNS data')
-                c.execute('INSERT INTO dns VALUES (?,?,?,?,?)',
-                            (timestamp2unixtime(data['timestamp']),
-                            data['dest_ip'], data['dns']['rrname'], data['dns']['rrtype'],
-                            data['dns']['rdata']))
-
-        # Store final counters of flow - UPDATE flow, set duration, counters, app_proto and erase flow_id
-        if data['event_type'] == 'flow' and data['flow'] and data['proto'] in ['TCP', 'UDP']:
-            logging.debug('Got flow!')
-            if 'app_proto' not in data.keys():
-                data['app_proto'] = 'unknown'
-            if data['app_proto'] in ['failed', 'dns'] or int(data['flow']['bytes_toserver'])==0 or int(data['flow']['bytes_toclient'])==0:
-                c.execute('DELETE FROM traffic WHERE flow_id = ?', (data['flow_id'],))
-                if c.rowcount!=1:
-                    logging.debug("Can't delete flow")
-            else:
-                c.execute('UPDATE traffic SET duration = ?, app_proto = ?, bytes_send = ?, bytes_received = ?, flow_id = NULL WHERE flow_id = ?', (int(timestamp2unixtime(data['flow']['end'])-timestamp2unixtime(data['flow']['start'])), data['app_proto'], data['flow']['bytes_toserver'], data['flow']['bytes_toclient'], data['flow_id']))
-                if c.rowcount!=1:
-                    logging.debug("Can't update flow")
-
-        # Store TLS details of flow - UPDATE flow, set hostname and app_proto
-        if data['event_type'] == 'tls' and data['tls']:
-            logging.debug('Got tls!')
-            hostname = ''
-            if 'sni' in data['tls'].keys():
-                hostname = data['tls']['sni']
-            elif 'subject' in data['tls'].keys():
-                hostname = data['tls']['subject']
-                #get only CN from suject
-                m = re.search('(?<=CN=)[^,]*', hostname)
-                if m:
-                     hostname = m.group(0)
-            if not hostname:
-                continue
-            c.execute('UPDATE traffic SET app_hostname = ?, app_proto = "tls" WHERE flow_id = ?', (hostname, data['flow_id']))
-            if c.rowcount!=1:
-                logging.debug("Can't update flow")
-
-        # Store HTTP details of flow - UPDATE flow, set hostname and app_proto
-        if data['event_type'] == 'http' and data['http']:
-            if 'hostname' not in data['http'].keys():
-                continue
-            c.execute('UPDATE traffic SET app_hostname = ?, app_proto = "http" WHERE flow_id = ?', (data['http']['hostname'], data['flow_id']))
-            if c.rowcount!=1:
-                logging.debug("Can't update flow")
-
-        # Store flow - INSERT with many fiels NULL/zeros
-        if data['event_type'] == 'flow_start' and data['flow'] and data['proto'] in ['TCP', 'UDP']:
-            if 'app_proto' not in data.keys():
-                data['app_proto'] = 'unknown'
-            if data['app_proto'] in ['failed', 'dns']:
-                continue
-            c.execute('INSERT INTO traffic (flow_id, start, src_mac, src_ip, src_port, dest_ip, dest_port, proto, app_proto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                        (data['flow_id'], timestamp2unixtime(data['flow']['start']),
-                        data['ether']['src'], data['src_ip'],
-                        data['src_port'], data['dest_ip'], data['dest_port'],
-                        data['proto'], data['app_proto']))
-        # Commit everything
+            handle_dns(data, c)
+        elif data['event_type'] == 'flow' and data['flow']:
+            handle_flow(data, c)
+        elif data['event_type'] == 'tls' and data['tls']:
+            handle_tls(data, c)
+        elif data['event_type'] == 'http' and data['http']:
+            handle_http(data, c)
+        elif data['event_type'] == 'flow_start' and data['flow']:
+            handle_flow_start(data, c)
+        else:
+            logging.warn("Unknown event type")
         con.commit()
 
     except KeyboardInterrupt:

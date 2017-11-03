efbb96566fcb5b0f6adc24228f52ee0481ba3721
Martin Petráček <martin.petracek@nic.cz>
2017-11-03 12:54:38 +0100

    show.py - nicer output

--- show.py
+++ show.py
@@ -20,12 +20,29 @@ except OSError:
     print("error calling dump.py")
     sys.exit(1)
 
-def timezone_offset():
-    is_dst = time.daylight and time.localtime().tm_isdst > 0
-    utc_offset = - (time.altzone if is_dst else time.timezone)
-    return utc_offset
+def print_table(table):
+    col_width = [max(len(str(x)) for x in col) for col in zip(*table)]
+    for line in table:
+        print ("|" + " | ".join("{:{}}".format(str(x), col_width[i])
+                                for i, x in enumerate(line)) + "|")
+
+def size_fmt(num):
+    for unit in ['','Ki','Mi','Gi']:
+        if abs(num) < 1024.0:
+            return "%3.0f%sB" % (num, unit)
+        num /= 1024.0
+    return "%.0f%sB" % (num, 'Ti')
 
 data=json.loads(response)
-for l in data:
-    l[0]=datetime.datetime.fromtimestamp(int(l[0])+timezone_offset()).strftime('%Y-%m-%d %H:%M:%S')
-    print(l)
+for i in range(len(data)):
+    data[i][0]=datetime.datetime.fromtimestamp(int(data[i][0])).strftime('%Y-%m-%d %H:%M:%S')
+    if data[i][1]==0:
+        data[i][1]="<1s"
+    else:
+        data[i][1]=str(data[i][1])+"s"
+    if data[i][3] and len(data[i][3])>40:
+        data[i][3]="..."+data[i][3][-40:]
+    data[i][6]=size_fmt(data[i][6])
+    data[i][7]=size_fmt(data[i][7])
+    data[i]=[str(c) for c in data[i]]
+print_table(data)

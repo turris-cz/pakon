3e3d0a3698cc6f2f9e7bd8c0def699d727af7835
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-04-29 10:59:35 +0200

    Remove create_db job.

--- pakon_light/cli/__init__.py
+++ pakon_light/cli/__init__.py
@@ -4,7 +4,6 @@
 #   See /LICENSE for more information.
 
 from .archive import main as archive
-from .create_db import main as create_db
 from .domains_reapply import main as domains_reapply
 from .handler import main as handler
 from .monitor import main as monitor

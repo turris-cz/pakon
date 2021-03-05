de5d7c3131666c5f0a81c8ec9f849a9b6908b1fc
Filip Hron <filip.hron@nic.cz>
2021-03-05 11:59:16 +0000

    fixup! backend commands changes, testing command parameters

--- pakon_api/test_api.py
+++ pakon_api/test_api.py
@@ -23,16 +23,20 @@ def _load_command_result(query):
     if not query:
         return data
     fltr = _split_query(query)
-    if 'number' in fltr:
-        retval = data[:fltr["number"]+1]
-        return retval
-    if 'page in fltr':
+    if 'page' in fltr:
         split = len(data)//2
-        return(data[:split], data[split:])[fltr['page']-1]
+        data = (data[:split], data[split:])[fltr['page']-1]
+    if 'number' in fltr:
+        data = data[:fltr["number"]+2]
+    return data
 
 
-@pytest.fixture
+@pytest.fixture(params="query")
 def query(request):
+    # TODO: make empty query default
+    # # see parametrize decorator of func `test_parser_ignores_empty_lines()`
+    if type(request.param) is not dict:
+        yield None
     yield _split_query(request.param)
 
 
@@ -68,7 +72,8 @@ def test_correct_subprocess_called(
     assert fake_process.calls[0] == cli_command
 
 
-def test_parser_ignores_empty_lines(client, fake_process):
+@pytest.mark.parametrize('query', [{}])
+def test_parser_ignores_empty_lines(client, fake_process, query):
     """ Test if cliParser class ignores empty values. """
     res = client.get(_URL)
     assert {} not in res.json
@@ -77,11 +82,12 @@ def test_parser_ignores_empty_lines(client, fake_process):
 @pytest.mark.parametrize(
     'query, exp', [
         ('?number=10', 10),
-        ('?page=2', 16)
+        ('?page=2', 24),
+        ('?page=2&number=5', 6)
     ]
 )
 def test_number_of_entries(client, fake_process, query, exp):
-    """ exp: expected result count """
+    """ exp: expected record return """
     url_call = _URL + query
     res = client.get(url_call)
     assert len(res.json) == exp

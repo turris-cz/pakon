e1630fb4a36072e25b7ea966b4e59db772ff0cf6
Filip Hron <filip.hron@nic.cz>
2021-11-01 21:59:42 +0100

    tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -26,7 +26,7 @@ FLOW1 = """<flow type="new">
     </meta>
 </flow>"""
 
-FLOW2 ="""<flow type="destroy">
+FLOW2 = """<flow type="destroy">
     <meta direction="original">
         <layer3 protonum="2" protoname="ipv4">
             <src>192.168.122.1</src>
@@ -123,3 +123,193 @@ FLOW4 = """<flow type="destroy">
         <unreplied/>
     </meta>
 </flow>"""
+
+JSON1 = """{
+  "flow": {
+    "meta": [
+      {
+        "layer3": {
+          "src": "192.168.16.40",
+          "dst": "192.168.16.255",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 60308,
+          "dport": 21027,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "direction": "original"
+      },
+      {
+        "layer3": {
+          "src": "192.168.16.255",
+          "dst": "192.168.16.40",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 21027,
+          "dport": 60308,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "direction": "reply"
+      },
+      {
+        "timeout": 30,
+        "id": 1657073971,
+        "unreplied": true,
+        "direction": "independent"
+      }
+    ],
+    "type": "new"
+  }
+}"""
+
+JSON2 = """{
+  "flow": {
+    "meta": [
+      {
+        "layer3": {
+          "src": "192.168.122.1",
+          "dst": "192.168.122.255",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 60308,
+          "dport": 21027,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "counters": {
+          "packets": 1,
+          "bytes": 648
+        },
+        "direction": "original"
+      },
+      {
+        "layer3": {
+          "src": "192.168.122.255",
+          "dst": "192.168.122.1",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 21027,
+          "dport": 60308,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "counters": {
+          "packets": 0,
+          "bytes": 0
+        },
+        "direction": "reply"
+      },
+      {
+        "id": 1043691933,
+        "unreplied": true,
+        "direction": "independent"
+      }
+    ],
+    "type": "destroy"
+  }
+}"""
+
+JSON3 = """{
+  "flow": {
+    "meta": [
+      {
+        "layer3": {
+          "src": "192.168.122.1",
+          "dst": "192.168.122.255",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 60308,
+          "dport": 21027,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "direction": "original"
+      },
+      {
+        "layer3": {
+          "src": "192.168.122.255",
+          "dst": "192.168.122.1",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 21027,
+          "dport": 60308,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "direction": "reply"
+      },
+      {
+        "timeout": 30,
+        "id": 990802630,
+        "unreplied": true,
+        "direction": "independent"
+      }
+    ],
+    "type": "new"
+  }
+}"""
+
+JSON4 = """{
+  "flow": {
+    "meta": [
+      {
+        "layer3": {
+          "src": "172.20.6.206",
+          "dst": "172.20.6.255",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 60308,
+          "dport": 21027,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "counters": {
+          "packets": 1,
+          "bytes": 648
+        },
+        "direction": "original"
+      },
+      {
+        "layer3": {
+          "src": "172.20.6.255",
+          "dst": "172.20.6.206",
+          "protonum": "2",
+          "protoname": "ipv4"
+        },
+        "layer4": {
+          "sport": 21027,
+          "dport": 60308,
+          "protonum": "17",
+          "protoname": "udp"
+        },
+        "counters": {
+          "packets": 0,
+          "bytes": 0
+        },
+        "direction": "reply"
+      },
+      {
+        "id": 3170238355,
+        "unreplied": true,
+        "direction": "independent"
+      }
+    ],
+    "type": "destroy"
+  }
+}"""
--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -1,12 +1,40 @@
 from json.encoder import py_encode_basestring
 import pytest
-from conftest import FLOW1, FLOW2, FLOW3, FLOW4
-from pakon.utils.xml_flow_parser import Parser
+from conftest import FLOW1, FLOW2, FLOW3, FLOW4, JSON1, JSON2, JSON3, JSON4
+from pakon.utils.xml_flow_parser import Parser, Array
 
-def test_whatever():
-    flow_root = Parser()
-    flow_root.parse(FLOW1)
-    breakpoint()
 
-def test_json():
-    pass
+def test_attributes():
+    xml = Parser()
+    xml.parse(FLOW1)
+    assert xml.root.flow.type == "new"
+    assert isinstance(xml.root.flow.meta, Array)
+    assert xml.root.flow.meta[0].direction == "original"
+    assert xml.root.flow.meta[0].layer3.protonum == "2"
+    assert xml.root.flow.meta[1].layer3.src.value == "192.168.16.255"
+    assert xml.root.flow.meta[2].id.value == 1657073971
+    assert xml.root.flow.meta[2].unreplied.value == True
+    assert xml.root.flow.meta[2].timeout.value == 30
+
+
+def test_port_attributes():
+    xml = Parser()
+    xml.parse(FLOW3)
+    assert xml.root.flow.meta[1].layer4.sport.value == 21027
+    assert xml.root.flow.meta[1].layer4.dport.value == 60308
+
+
+def test_counter_attributes():
+    xml = Parser()
+    xml.parse(FLOW4)
+    assert xml.root.flow.meta[0].counters.packets.value == 1
+    assert xml.root.flow.meta[0].counters.bytes.value == 648
+
+
+@pytest.mark.parametrize(
+    "source,target", [(FLOW1, JSON1), (FLOW2, JSON2), (FLOW3, JSON3), (FLOW4, JSON4)]
+)
+def test_json(source, target):
+    xml = Parser()
+    xml.parse(source)
+    assert xml.jsonify() == target
